6d0a918ffa62b06e2c5511f9142ddc5c6a1f875b
Илья Владимиров <flashwot@yandex.ru>
2023-05-01 20:27:50 +0300

    fix background

--- 2_task/src/components/Background.jsx
+++ 2_task/src/components/Background.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 
 const Background = () => {
+  window.addEventListener('scroll', () => {
+    document.querySelector('.background').style.top = `${window,scrollY * -0.5}px`;
+  })
+
   return (
     <div className="background">
       <div className="circle circle1"></div>
